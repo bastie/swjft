f5abbe05327070013b4b97bfbcac4d02ea35c319
Sͬeͥbͭaͭsͤtͬian <Sebnospam@example.com>
2016-07-16 14:20:47 +0200

    Exceptions <= jdk6)

--- swjft/swjft/lang/Exception.swift
+++ swjft/swjft/lang/Exception.swift
@@ -10,8 +10,45 @@ import Foundation
 
 extension lang {
 
-	public class Exception : Throwable, ErrorType {
-    // NOT YET IMPLEMENTED
+  // NOT YET IMPLEMENTED
+	public class Exception : Throwable {
+    
+
+    public override init(){super.init()}
+    
+    public override init(message : lang.String) {
+      super.init(message: message)
+    }
+    // Helper method
+    public override init(detailMessage : Swift.String) {
+      super.init(message: lang.String(otherString: detailMessage))
+    }
+    
+    public override init (message : lang.String, cause : lang.Throwable) {
+      super.init (message : message, cause : cause)
+    }
+    
+    public override init (detailMessage : Swift.String, cause : lang.Throwable) {
+      super.init(message: lang.String(otherString: detailMessage), cause : cause)
+    }
+
+    public override init (cause : lang.Throwable) {
+      super.init(message: cause.toStringJ(), cause : cause)
+    }
+    /* NOT YET IMPLEMENTED - JDK 7
+    public init (message : lang.String, cause : lang.Throwable, enableSuppression : Bool, writableStackTrace: Bool) {
+      self.detailMessage = message
+      self.cause = cause
+      self.suppressionEnable = enableSuppression
+      self.stackTraceWritable = writableStackTrace
+    }
+    
+    public init (detailMessage : Swift.String, cause : lang.Throwable, enableSuppression : Bool, writableStackTrace: Bool) {
+      self.detailMessage = lang.String(otherString: detailMessage)
+      self.cause = cause
+      self.suppressionEnable = enableSuppression
+      self.stackTraceWritable = writableStackTrace
+    }*/
   }
     
 }
--- swjft/swjft/lang/Throwable.swift
+++ swjft/swjft/lang/Throwable.swift
@@ -7,7 +7,46 @@
 //
 
 extension lang {
-    public class Throwable : Object {
-        public override init (){}
-    }
+    public class Throwable : Object, ErrorType {
+      private var detailMessage : lang.String?
+      private var cause : lang.Throwable?
+
+      public override init (){}
+      public init(message : lang.String) {
+        self.detailMessage = message
+      }
+      
+      public init(detailMessage : Swift.String) {
+        self.detailMessage = lang.String(otherString: detailMessage)
+      }
+      
+      public init (message : lang.String, cause : lang.Throwable) {
+        self.detailMessage = message
+        self.cause = cause
+      }
+      
+      public init (detailMessage : Swift.String, cause : lang.Throwable) {
+        self.detailMessage = lang.String(otherString: detailMessage)
+        self.cause = cause
+      }
+      
+      public init (cause : lang.Throwable) {
+        self.detailMessage = cause.toStringJ()
+        self.cause = cause
+      }
+      /* NOT YET IMPLEMENTED - JDK 7
+      public init (message : lang.String, cause : lang.Throwable, enableSuppression : Bool, writableStackTrace: Bool) {
+        self.detailMessage = message
+        self.cause = cause
+        self.suppressionEnable = enableSuppression
+        self.stackTraceWritable = writableStackTrace
+      }
+      
+      public init (detailMessage : Swift.String, cause : lang.Throwable, enableSuppression : Bool, writableStackTrace: Bool) {
+        self.detailMessage = lang.String(otherString: detailMessage)
+        self.cause = cause
+        self.suppressionEnable = enableSuppression
+        self.stackTraceWritable = writableStackTrace
+      }*/
+  }
 }
